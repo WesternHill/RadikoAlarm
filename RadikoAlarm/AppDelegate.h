a48a664d18feb8ea043db9638a8303976e5e1178
tetsurou <root@Teturou-MacPro.local>
2016-05-09 20:16:05 +0900

    last change

--- RadikoAlarm/AppDelegate.h
+++ RadikoAlarm/AppDelegate.h
@@ -8,8 +8,29 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSViewController <NSApplicationDelegate,NSDatePickerCellDelegate>{
+	IBOutlet NSButton *bootRadikoButton;
+	IBOutlet NSButton *killRadikoButton;
+	IBOutlet NSTextField *clkField;
+	IBOutlet NSTextField *dateField;
+	IBOutlet NSDatePicker *startTime;
+	IBOutlet NSDatePicker *endTime;
+	IBOutlet NSButton *sun;
+	IBOutlet NSButton *mon;
+	IBOutlet NSButton *tue;
+	IBOutlet NSButton *wed;
+	IBOutlet NSButton *thr;
+	IBOutlet NSButton *fri;
+	IBOutlet NSButton *sat;
+
+	NSTimer *timer;
+}
+
+-(void)bootRadiko;
+-(void)killRadiko;
 
 @property (assign) IBOutlet NSWindow *window;
+@property (readwrite) IBOutlet NSButton *bootRadikoButton;
+@property (readwrite) IBOutlet NSButton *killRadikoButton;
 
 @end
